2a9ce5a24d7735539f9d7d4416ba2c7239a707cc
Herman Bergwerf <hermanbergwerf@gmail.com>
2016-07-26 11:33:57 -0500

    Fix json loads

--- modules/json.js
+++ modules/json.js
@@ -12,7 +12,7 @@ var $builtinmodule = function (name) {
 
   // Fetch API
   mod.loads = new Sk.builtin.func(function (str) {
-    var json = JSON.parse(str)
+    var json = JSON.parse(str.v)
     __logJson(json)
     return Sk.ffi.remapToPy(json)
   })
